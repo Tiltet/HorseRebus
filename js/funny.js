20c1751ef3be12a697800dae31a8715d7156595c
Tiltet <114957669+Tiltet@users.noreply.github.com>
2023-03-03 11:04:30 +0300

    Add files via upload

--- js/funny.js
+++ js/funny.js
@@ -4,7 +4,7 @@ let button__start = document.getElementById('button__start');
 let button__stop = document.getElementById('button__stop');
 
 const countSpan = document.querySelector('.counter__count');
-let count = 900;
+let count = 900; // 1
 
 let interval;
 
@@ -24,22 +24,25 @@ button__stop.addEventListener('click', () => {
 
 // ФУНКЦИИ ДЛЯ КНОПОК START И STOP
 
+// 1 = 900
+// 2 = 800
+// 9 = 100
 
 // ФУНКЦИЯ ДЛЯ СЧЕТЧИКА СКОРОСТИ ХОДА
 const plusBtn = document.querySelector('.counter__plus');
 const minusBtn = document.querySelector('.counter__minus');
 
 plusBtn.addEventListener('click', () => {
-   if (count < 900) {
-      count = count + 100;
-      countSpan.textContent = count;
+   if (count > 100) {
+      count = count - 100;
+      countSpan.textContent = (10 - Math.floor(count / 100));
    }
 });
 
 minusBtn.addEventListener('click', () => {
-   if (count > 100) {
-      count = count - 100;
-      countSpan.textContent = count;
+   if (count < 900) {
+      count = count + 100;
+      countSpan.textContent = (10 - Math.floor(count / 100));
    }
 });
 // ФУНКЦИЯ ДЛЯ СЧЕТЧИКА СКОРОСТИ ХОДА
